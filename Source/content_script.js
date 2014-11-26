84d22322fa1758aa87cf913a32d23cd4015ecad1
vaguerant <vaguerant@users.noreply.github.com>
2014-11-27 02:20:30 +1100

    Meating it up.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bMeta(?!l)/g, "Meat");
+	v = v.replace(/\bmeta(?!l)/g, "meat");
+	v = v.replace(/\bMETA(?!l)/g, "MEAT");
 	
 	textNode.nodeValue = v;
 }
