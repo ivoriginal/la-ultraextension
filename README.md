a8f379d7b6912e95109789a0e7b804082704f710
pau valiente <mail@pau.cc>
2019-04-29 12:32:38 +0200

    Primer commit de la ultraextensión

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-Metameater
+La ultraextensión
 ==========
 
-Opera 15+ (and therefore Chrome, probably) extension that replaces occurrences of 'meta' with 'meat'. An exception is made if the next letter is "L" to avoid things like "heavy meatl" from happening.
+En las elecciones generales españolas de Abril de 2019 un partido de la ultraderecha ha conseguido representación en el congreso de los diputados. Una parte de la culpa la han tenido los medios que hicieron de altavoz de ese partido durante la campaña electoral, repitiendo cada burrada que decían, y también la gente en las redes sociales que lo repetía indignándose.
 
->"Meanwhile, officials at Queensland Police began accessing the private meatdata of cadets last July to determine whether they were sleeping with one another or faking sick days."
+Ahora, desde la oposición van a tener aún más voz, así que para no normalizar la ultraderecha asociándola a tres letras como si fuera un partido más, he creado esta extensión que modifica el nombre de ese partido por "la ultraderecha".
+
+
+
+**Nota:** La extensión parece no funcionar en el nuevo twitter, si alguien sabe cómo arreglarlo, estaré encantado de modificarla.
--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
 	if (node.nodeName.toLowerCase() == 'input' || node.nodeName.toLowerCase() == 'textarea'
-	    || (node.classList && node.classList.contains('ace_editor'))) {
-		 return;
+			|| (node.classList && node.classList.contains('ace_editor'))) {
+			return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,15 +32,17 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bMeta(?!l)/g, "Meat");
-	v = v.replace(/\bmeta(?!l)/g, "meat");
-	v = v.replace(/\bMETA(?!L)/g, "MEAT");
-	
+	v = v.replace(/\bVox\b/g, "la ultraderecha");
+	v = v.replace(/\bvox\b/g, "la ultraderecha");
+	v = v.replace(/\bVOX\b/g, "la ultraderecha");
+	v = v.replace(/\bVOx\b/g, "la ultraderecha");
+	v = v.replace(/\bV0X\b/g, "la ultraderecha");
+	v = v.replace(/\bv0x\b/g, "la ultraderecha");
+	v = v.replace(/\bV0x\b/g, "la ultraderecha");
+
 	textNode.nodeValue = v;
 }
-
-
